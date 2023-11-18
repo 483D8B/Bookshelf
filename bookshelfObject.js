58d97aadd1e30fed3dc75ac4ce37fa75b94f6356
christian <134725438+483D8B@users.noreply.github.com>
2023-11-18 18:40:19 +0100

    Create bookshelfObject.js

---
+++ bookshelfObject.js
@@ -0,0 +1,14 @@
+const isbnColorMap = {
+  '9788807900761': { color: '#353439', title: '#FFF' },
+  '9780465078356': { color: '#5F5761', title: '#FFF' },
+  '9788811363347': { color: '#28264E', title: '#FFF' },
+  '9788809950542': { color: '#A88294', title: '#FFF' },
+  '9788850222452': { color: '#0A0A0A', title: '#FFF' },
+  '9788817147347': { color: '#F3DB39', title: '#000' },
+  '9788845296499': { color: '#FFFFFF', title: '#000' },
+  '9788845272400': { color: '#01080B', title: '#FFF' },
+  '9788807091766': { color: '#AB0840', title: '#FFF' },
+  '9788804567028': { color: '#EAECED', title: '#000' },
+  '9788842822363': { color: '#C2C4CE', title: '#000' },
+ 
+};
